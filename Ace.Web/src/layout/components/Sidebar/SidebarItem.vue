ce3184fb63c47e01b7c5f86dcb9e2005e2fd7347
JackChen <121955443@qq.com>
2019-12-08 14:39:17 +0800

    update

--- Ace.Web/src/layout/components/Sidebar/SidebarItem.vue
+++ Ace.Web/src/layout/components/Sidebar/SidebarItem.vue
@@ -1,23 +1,20 @@
 <template>
-  <div v-if="!item.hidden" class="menu-wrapper">
-    <template v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
-      <app-link v-if="onlyOneChild.meta" :to="resolvePath(onlyOneChild.path)">
-        <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
-          <item :icon="onlyOneChild.meta.icon||(item.meta&&item.meta.icon)" :title="onlyOneChild.meta.title" />
-        </el-menu-item>
-      </app-link>
+  <div class="menu-wrapper">
+    <template v-if="item.children.length===0">
+      <el-menu-item :index="String(item.id)" :class="{'submenu-title-noDropdown':!isNest}">
+        <item :title="item.name"/>
+      </el-menu-item>
     </template>
 
-    <el-submenu v-else ref="subMenu" :index="resolvePath(item.path)" popper-append-to-body>
+    <el-submenu v-else :index="String(item.id)">
       <template slot="title">
-        <item v-if="item.meta" :icon="item.meta && item.meta.icon" :title="item.meta.title" />
+        <item :title="item.name"/>
       </template>
       <sidebar-item
         v-for="child in item.children"
-        :key="child.path"
+        :key="child.id"
         :is-nest="true"
         :item="child"
-        :base-path="resolvePath(child.path)"
         class="nest-menu"
       />
     </el-submenu>
