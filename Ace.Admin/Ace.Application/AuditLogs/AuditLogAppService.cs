2cdf897958411e9c30a23c77b82665a95f6c711b
JackChen <121955443@qq.com>
2019-12-19 00:01:57 +0800

    update

--- Ace.Admin/Ace.Application/AuditLogs/AuditLogAppService.cs
+++ Ace.Admin/Ace.Application/AuditLogs/AuditLogAppService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
 using Abp.Application.Services;
 using Abp.Application.Services.Dto;
@@ -9,6 +10,7 @@ using Abp.Extensions;
 using Abp.Linq.Extensions;
 using Ace.AuditLogs.Dto;
 using Ace.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ace.AuditLogs
 {
@@ -20,32 +22,30 @@ namespace Ace.AuditLogs
     public class AuditLogAppService : ApplicationService
     {
         IRepository<AuditLog, long> _repository;
+
         public AuditLogAppService(
             IRepository<AuditLog, long> repository)
         {
             _repository = repository;
         }
 
-        public Task<PagedResultDto<AuditLogDto>> GetAll(AuditLogQuery input)
+        public async Task<PagedResultDto<AuditLogDto>> GetAll(AuditLogQuery input)
         {
-            return Task.Run(() =>
-            {
-                var logQuery = _repository.GetAll()
-                       .WhereIf(input.UserId.HasValue, p => p.UserId == input.UserId)
-                       .WhereIf(!input.ClientIpAddress.IsNullOrWhiteSpace(), p => p.ClientIpAddress.Contains(input.ClientIpAddress))
-                       .WhereIf(!input.Keyword.IsNullOrWhiteSpace(), p => p.ServiceName.Contains(input.Keyword) || p.MethodName.Contains(input.Keyword))
-                       .WhereIf(input.StartTime.HasValue, x => x.ExecutionTime > input.StartTime)
-                       .WhereIf(input.EndTime.HasValue, x => x.ExecutionTime < input.EndTime)
-                       ;
-                var totalCount = logQuery.Count();
-                logQuery = logQuery.OrderBy(p => p.ExecutionTime);
-                logQuery = logQuery.Skip(input.SkipCount).Take(input.MaxResultCount);
-                return new PagedResultDto<AuditLogDto>
-                {
-                    TotalCount = totalCount,
-                    Items = logQuery.Select(s => ObjectMapper.Map<AuditLogDto>(s)).ToList()
-                };
-            });
+            var query = _repository.GetAll()
+                   .WhereIf(input.UserId.HasValue, p => p.UserId == input.UserId)
+                   .WhereIf(!input.ClientIpAddress.IsNullOrWhiteSpace(), p => p.ClientIpAddress.Contains(input.ClientIpAddress))
+                   .WhereIf(!input.Keyword.IsNullOrWhiteSpace(), p => p.ServiceName.Contains(input.Keyword) || p.MethodName.Contains(input.Keyword))
+                   .WhereIf(input.StartTime.HasValue, x => x.ExecutionTime > input.StartTime)
+                   .WhereIf(input.EndTime.HasValue, x => x.ExecutionTime < input.EndTime)
+                   ;
+            var totalCount = await query.CountAsync();
+            query = query.OrderBy(input.Sorting);
+            query = query.Skip(input.SkipCount).Take(input.MaxResultCount);
+            var entities = await query.ToListAsync();
+            return new PagedResultDto<AuditLogDto>(
+                totalCount,
+                entities.Select(s => ObjectMapper.Map<AuditLogDto>(s)).ToList()
+            );
         }
     }
 }
