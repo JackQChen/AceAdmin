78d3ea0d268f4cf350cf7f6d11334151f97181d6
JackChen <121955443@qq.com>
2020-01-04 20:18:16 +0800

    update

--- Ace.Web/src/views/menu/index.vue
+++ Ace.Web/src/views/menu/index.vue
@@ -10,6 +10,7 @@
             v-loading="loading"
             :data="menuTree"
             :props="defaultProps"
+            :expand-on-click-node="false"
             default-expand-all
             @node-click="handleNodeClick">
             <span slot-scope="{ data }" class="custom-tree-node">
@@ -30,15 +31,38 @@
             <el-input v-model="menu.name"/>
           </el-form-item>
           <el-form-item label="上级菜单">
-            <el-input v-model="menu.parentId"/>
+            <el-cascader
+              v-model="menu.parentId"
+              :options="menuTree"
+              :props="defaultProps"
+              clearable
+              style="width:100%">
+              <template slot-scope="{ data }">
+                <svg-icon :icon-class="data.icon?data.icon:'menu'"/>
+                <span>{{ data.name }}</span>
+              </template>
+            </el-cascader>
           </el-form-item>
           <el-form-item label="图标">
             <el-input v-model="menu.icon">
               <template slot="prepend"><svg-icon :icon-class="menu.icon?menu.icon:'menu'" /></template>
             </el-input>
           </el-form-item>
+          <el-form-item label="模块">
+            <el-cascader
+              v-model="menu.parentId"
+              :options="menuTree"
+              :props="defaultProps"
+              clearable
+              style="width:100%">
+              <template slot-scope="{ data }">
+                <svg-icon :icon-class="data.icon?data.icon:'menu'"/>
+                <span>{{ data.name }}</span>
+              </template>
+            </el-cascader>
+          </el-form-item>
           <el-form-item label="排序">
-            <el-input v-model="menu.order"/>
+            <el-input-number v-model="menu.order" :min="0" />
           </el-form-item>
           <el-form-item label="启用">
             <el-switch v-model="menu.isActive"/>
@@ -65,7 +89,9 @@ export default {
       menu: {},
       defaultProps: {
         children: 'children',
-        label: 'name'
+        value: 'id',
+        label: 'name',
+        expandTrigger: 'hover'
       }
     }
   },
@@ -75,9 +101,19 @@ export default {
   methods: {
     fetchData() {
       getMenuTree(this.listQuery).then(data => {
-        this.menuTree = data
+        this.menuTree = this.getTreeData(data)
         this.loading = false
       })
+    },
+    getTreeData(data) {
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].children.length == 0) {
+          data[i].children = null
+        } else {
+          this.getTreeData(data[i].children)
+        }
+      }
+      return data
     },
     handleNodeClick(data) {
       this.menu = data
@@ -89,6 +125,9 @@ export default {
 .el-tree-node__content
 {
   height: 36px;
+}
+.el-cascader-menu__hover-zone path{
+    fill:transparent !important;
 }
 </style>
 <style lang="scss" scoped>
