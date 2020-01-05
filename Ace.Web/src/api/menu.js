c61f6f451211dc3802be6640d2340505cbe4e5e1
JackChen <121955443@qq.com>
2020-01-05 12:25:30 +0800

    update

--- Ace.Web/src/api/menu.js
+++ Ace.Web/src/api/menu.js
@@ -7,3 +7,27 @@ export function getMenuTree(params) {
     params
   })
 }
+
+export function createMenu(data) {
+  return request({
+    url: 'Menu/Create',
+    method: 'post',
+    data
+  })
+}
+
+export function updateMenu(data) {
+  return request({
+    url: 'Menu/Update',
+    method: 'put',
+    data
+  })
+}
+
+export function deleteMenu(params) {
+  return request({
+    url: 'Menu/Delete',
+    method: 'delete',
+    params
+  })
+}
--- Ace.Web/src/views/menu/index.vue
+++ Ace.Web/src/views/menu/index.vue
@@ -4,6 +4,10 @@
       <el-card class="box-card">
         <div slot="header">
           <span>菜单结构</span>
+          <el-button-group style="float:right;margin-top:-8px;">
+            <el-button type="primary" size="small" icon="el-icon-plus" @click="addMenu"/>
+            <el-button type="primary" size="small" icon="el-icon-delete" @click="removeMenu"/>
+          </el-button-group>
         </div>
         <div class="fill">
           <el-tree
@@ -27,18 +31,24 @@
           <span>菜单信息</span>
         </div>
         <el-form ref="form" :model="menu" label-width="100px" style="width:500px">
+          <el-form-item label="菜单ID">
+            <el-input v-model="menu.id" :disabled="true"/>
+          </el-form-item>
           <el-form-item label="菜单名称">
             <el-input v-model="menu.name"/>
           </el-form-item>
           <el-form-item label="上级菜单">
             <el-cascader
-              v-model="menu.parentId"
+              ref="parentMenu"
               :options="menuTree"
               :props="defaultProps"
+              :value="parentPath"
+              placeholder="请选择上级菜单"
               clearable
-              style="width:100%">
+              style="width:100%"
+              @change="parentChange">
               <template slot-scope="{ data }">
-                <svg-icon :icon-class="data.icon?data.icon:'menu'"/>
+                <svg-icon :icon-class="data.icon?data.icon:'menu'" />
                 <span>{{ data.name }}</span>
               </template>
             </el-cascader>
@@ -50,8 +60,6 @@
           </el-form-item>
           <el-form-item label="模块">
             <el-cascader
-              v-model="menu.parentId"
-              :options="menuTree"
               :props="defaultProps"
               clearable
               style="width:100%">
@@ -68,7 +76,7 @@
             <el-switch v-model="menu.isActive"/>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary">保存</el-button>
+            <el-button type="primary" @click="save">保存</el-button>
           </el-form-item>
         </el-form>
       </el-card>
@@ -77,20 +85,23 @@
 </template>
 <script>
 import SplitPane from 'vue-splitpane'
-import { getMenuTree } from '@/api/menu'
+import Message from 'element-ui'
+import { getMenuTree, createMenu, updateMenu, deleteMenu } from '@/api/menu'
 
 export default {
-  components: { SplitPane },
+  components: { SplitPane, Message },
   data() {
     return {
       loading: true,
       menuTree: [],
       menu: {},
+      parentPath: [],
       defaultProps: {
         children: 'children',
         value: 'id',
         label: 'name',
-        expandTrigger: 'hover'
+        expandTrigger: 'hover',
+        checkStrictly: true
       }
     }
   },
@@ -100,22 +111,51 @@ export default {
   methods: {
     fetchData() {
       getMenuTree(this.listQuery).then(data => {
-        this.menuTree = this.getTreeData(data)
+        this.menuTree = this.removeEmptyNode(data)
         this.loading = false
       })
     },
-    getTreeData(data) {
+    removeEmptyNode(data) {
       for (var i = 0; i < data.length; i++) {
         if (data[i].children.length === 0) {
           data[i].children = null
         } else {
-          this.getTreeData(data[i].children)
+          this.removeEmptyNode(data[i].children)
         }
       }
       return data
     },
-    handleNodeClick(data) {
+    handleNodeClick(data, node) {
       this.menu = data
+      const path = []
+      this.getParentPath(path, node.parent)
+      path.reverse()
+      this.parentPath = path
+    },
+    getParentPath(path, node) {
+      if (node.data.id) {
+        path.push(node.data.id)
+        this.getParentPath(path, node.parent)
+      }
+    },
+    parentChange() {
+      this.menu.parentId = this.parentPath[this.parentPath.length - 1]
+      this.$refs.parentMenu.toggleDropDownVisible()
+    },
+    addMenu() {},
+    removeMenu() {
+      deleteMenu()
+    },
+    save() {
+      if (this.menu.id) {
+        updateMenu(this.menu).then(data => {
+          Message({
+            message: 'done'
+          })
+        })
+      } else {
+        createMenu(this.menu)
+      }
     }
   }
 }
@@ -125,8 +165,21 @@ export default {
 {
   height: 36px;
 }
-.el-cascader-menu__hover-zone {
+.el-cascader-menu__hover-zone
+{
     display: none;
+}
+.el-cascader-panel .el-radio
+{
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+  position: absolute;
+  left: 0;
+}
+.el-cascader-panel .el-radio__input
+{
+  visibility: hidden;
 }
 </style>
 <style lang="scss" scoped>
